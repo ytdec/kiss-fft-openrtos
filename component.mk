ae351bf559545d3e90bee99de548d056dd918109
ytdec <>
2019-12-25 16:47:49 +0100

    change player -> fft in component.mk

--- component.mk
+++ component.mk
@@ -1,7 +1,7 @@
-INC_DIRS += $(player_ROOT)
+INC_DIRS += $(fft_ROOT)
 # args for passing into compile rule generation
-player_SRC_DIR =  $(player_ROOT)
-player_CFLAGS = -lm
-player_LDFLAGS = -lm
-player_LIBS = m
-$(eval $(call component_compile_rules,player))
+fft_SRC_DIR =  $(fft_ROOT)
+fft_CFLAGS = -lm
+fft_LDFLAGS = -lm
+fft_LIBS = m
+$(eval $(call component_compile_rules,fft))
